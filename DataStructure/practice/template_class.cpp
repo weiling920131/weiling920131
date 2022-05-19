5c6da4b3e157b48d20c9dbd58db1dcd8a37cc96e
weiling <weiling920131@gmail.com>
2022-05-19 15:58:01 +0800

    DataStructure

---
+++ DataStructure/practice/template_class.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+template < class T >
+class Node{
+public:
+T& add(T& a){
+    return *this;
+}
+};
+
+int main(){
+
+
+
+    system("pause");
+    return 0;
+}
+
+
+
+
